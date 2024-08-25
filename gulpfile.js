c223daac647a8d3f2441cc76853640ec008ff13a
HXHdevs <jordisonnicolas@gmail.com>
2024-08-24 23:31:40 -0600

    Compilando SASS con Gulp checar el commit y README.md

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
-export function hola( done ){
-    console.log('Hola desde GulpFile.js')
-    
-    done() //Agregamos done() para hacerle saber que ya se termino la tarea
+import {src,dest} from 'gulp'
+import * as dartSass from 'sass'
+import gulpSass from 'gulp-sass'
+
+const sass = gulpSass(dartSass)
+
+export function css(done){
+    src('src/scss/app.css')
+        .pipe(sass())
+        .pipe(dest('build/css'))
+    done()
 }
